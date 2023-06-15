2dd9460eade4152a270b995f9f65a2c8678b58c4
oliver23n <oliver.nasevski@gmail.com>
2023-06-15 18:36:42 -0400

    created router

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,69 @@
-// const express = require('express');
-// const router = express.Router();
-// const fs = require('fs');
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const uuid = require('../helpers/uuid');
+
+
+
+router.get('/',(req,res)=> {
+
+    fs.readFile('./db/db.json', (err, data) => err ? console.error(err) : res.status(200).json(JSON.parse(data)));
+
+    });
+
+router.post('/', (req, res) => {
+
+    const {title, text} = req.body;
+    if(req.body && title && text){
+        const newNote = {
+            title,
+            text,
+            id:uuid()
+        };
+
+
+        fs.readFile('./db/db.json', (err,data) =>{
+            if(err){
+                console.error(err);
+            }else{
+                let json = JSON.parse(data);
+                json.push(newNote);
+
+                fs.writeFile('./db/db.json',JSON.stringify(json), (eror) => eror ? console.error(eror):console.log(`the note ${JSON.stringify(newNote)} has been added to file`));
+            }});
+
+        res.status(201).json(newNote);
+    } else {
+        res.status(500).json('Error');
+    }
+
+});
+
+router.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    let index;
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            let json = JSON.parse(data);
+            for (let i = 0; i < json.length; i++) {
+                const currentId = json[i].id;
+                if (currentId === noteId) {
+                    index = i;
+                };
+            };
+            json.splice(index, 1);
+            fs.writeFile('./db/db.json', JSON.stringify(json), (eror) => eror ? console.error(eror) : console.log(`the note with the id of ${noteId} has been deleted`));
+        }
+    })
+    res.redirect('/')
+
+})
 
 
 
 
 
 
-// module.export = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3001;
-const uuid = require('./helpers/uuid');
 
-const fs = require('fs');
+// const uuid = require('./helpers/uuid');
+// const fs = require('fs');
+const api = require('./routes/notes')
+
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/notes',api);
 
 
 app.get('/', (req,res) => {
@@ -18,63 +21,65 @@ app.get('/notes', (req,res)=> {
     res.sendFile(path.join(__dirname ,'/public/notes.html'));
 })
 
-app.get('/api/notes',(req,res)=> {
+// app.get('/api/notes',(req,res)=> {
 
-    fs.readFile('./db/db.json', (err, data) => err ? console.error(err) : res.status(200).json(JSON.parse(data)));
+//     fs.readFile('./db/db.json', (err, data) => err ? console.error(err) : res.status(200).json(JSON.parse(data)));
 
-    });
+//     });
     
 
 
-app.post('/api/notes',(req,res) =>{
+// app.post('/api/notes',(req,res) =>{
 
-    const {title, text} = req.body;
-    if(req.body && title && text){
-        const newNote = {
-            title,
-            text,
-            id:uuid()
-        };
+//     const {title, text} = req.body;
+//     if(req.body && title && text){
+//         const newNote = {
+//             title,
+//             text,
+//             id:uuid()
+//         };
 
 
-        fs.readFile('./db/db.json', (err,data) =>{
-            if(err){
-                console.error(err);
-            }else{
-                let json = JSON.parse(data);
-                json.push(newNote);
+//         fs.readFile('./db/db.json', (err,data) =>{
+//             if(err){
+//                 console.error(err);
+//             }else{
+//                 let json = JSON.parse(data);
+//                 json.push(newNote);
 
-                fs.writeFile('./db/db.json',JSON.stringify(json), (eror) => eror ? console.error(eror):console.log(`the note ${JSON.stringify(newNote)} has been added to file`));
-            }});
+//                 fs.writeFile('./db/db.json',JSON.stringify(json), (eror) => eror ? console.error(eror):console.log(`the note ${JSON.stringify(newNote)} has been added to file`));
+//             }});
 
-        res.status(201).json(newNote);
-    } else {
-        res.status(500).json('Error');
-    }
+//         res.status(201).json(newNote);
+//     } else {
+//         res.status(500).json('Error');
+//     }
 
-});
+// });
 
-app.delete('/api/notes/:id', (req,res) => {
-    const noteId = req.params.id;
-    let index;
-    fs.readFile('./db/db.json', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            let json = JSON.parse(data);
-            for(let i = 0; i<json.length; i++){
-                const currentId = json[i].id;
-                if(currentId === noteId){
-                    index = i;
-                };
-            };
-            json.splice(index,1);
-            fs.writeFile('./db/db.json', JSON.stringify(json), (eror) => eror ? console.error(eror) : console.log(`the note with the id of ${noteId} has been deleted`)); 
-        }
-    })
-    res.status(201).json("completed");
+// app.delete('/api/notes/:id', (req,res) => {
+//     const noteId = req.params.id;
+//     let index;
+//     fs.readFile('./db/db.json', (err, data) => {
+//         if (err) {
+//             console.error(err);
+//         } else {
+//             let json = JSON.parse(data);
+//             for(let i = 0; i<json.length; i++){
+//                 const currentId = json[i].id;
+//                 if(currentId === noteId){
+//                     index = i;
+//                 };
+//             };
+//             json.splice(index,1);
+//             fs.writeFile('./db/db.json', JSON.stringify(json), (eror) => eror ? console.error(eror) : console.log(`the note with the id of ${noteId} has been deleted`)); 
+//             res.redirect('/');
+//         }
+//     })
 
-    })
+
+//     })
+
 
 app.listen(PORT, ()=> {
     console.log(`listening to Port : ${PORT}`);
